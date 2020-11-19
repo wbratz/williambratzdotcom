18f8dd8ab385aa6d9b362a7e71f79e3e08970304
Billy <williambratz00@gmail.com>
2020-11-19 08:13:35 -0600

    Adds header and logo logo still needs formatting

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,6 +9,16 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <header className={styles.header}>
+        <div className={styles.headerRight}>
+          <a href="#techStack">Tech Stack</a>
+        </div>
+      </header>
+
+      <div className={styles.logo}>
+      <img src="/headerPhoto.jpg" alt="Handsome"/>
+      </div>
+
       <main className={styles.main}>
         William Bratz dot com
       </main>
