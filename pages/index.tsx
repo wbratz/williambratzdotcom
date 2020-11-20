f13d8fc1ef3f59077f382fcc5111f7299940f23a
Billy <williambratz00@gmail.com>
2020-11-20 07:51:10 -0600

    adds slide out menu

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,24 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import react, { useState } from "react";
 
 export default function Home() {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const openNav = () => {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.left = "250px";
+    setNavIsOpen(true);
+  }
+
+  const closeNav = () => {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    setNavIsOpen(false);
+  }
+
   return (
+    <div id="main">
     <div className={styles.container}>
       <Head>
         <title>William Bratz dot com</title>
@@ -10,14 +26,21 @@ export default function Home() {
       </Head>
 
       <header className={styles.header}>
-        <div className={styles.headerRight}>
-          <a href="#techStack">Tech Stack</a>
-        </div>
-      </header>
-
       <div className={styles.logo}>
       <img src="/headerPhoto.jpg" alt="Handsome"/>
       </div>
+        <div className={styles.headerRight}>
+          <a href="javascript:void(0)" onClick={navIsOpen ? closeNav : openNav}>Tech Stack</a>
+        </div>
+      </header>
+
+      <div id="mySidenav" className={styles.sidenav}>
+        <a href="javascript:void(0)" className={styles.closebtn} onClick={closeNav}>&times;</a>
+        <a href="https://nextjs.org/">Next JS</a>
+        <a href="https://reactjs.org/">React</a>
+        <a href="https://heroku.com/">Heroku</a>
+      </div>
+      
 
       <main className={styles.main}>
         William Bratz dot com
@@ -26,6 +49,7 @@ export default function Home() {
       <footer className={styles.footer}>
         
       </footer>
+    </div>
     </div>
   );
 }
