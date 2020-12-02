7e7a20611011848078fb9a9b2f60d431a523cbf9
William Bratz <williambratz615@gmail.com>
2020-12-02 16:15:47 -0500

    basic responsive navbar, still needs refinement, but works for now (#10)
    Co-authored-by: Billy <williambratz00@gmail.com>

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,59 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
 import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
-export default function Header({navIsOpen, setNavIsOpen}) {
+export default function Header({ navIsOpen, setNavIsOpen }) {
+  const [mobileNavOpen, setMobileNavOpen] = useState(false);
 
-      return(
-        <>
-        <Head>
+  const setNavStyle = () => {
+    setMobileNavOpen(!mobileNavOpen);
+  };
+
+  return (
+    <>
+      <Head>
         <title>William Bratz dot com</title>
         <link rel="icon" href="/newIcon.ico" />
-         </Head>
+      </Head>
 
-        <header className={styles.header}>
-          <div className={styles.logo}>
-            <Link href="/"><a><img src="/headerphoto.jpg" alt="Handsome"/></a></Link>
-          </div>
-        <div className={styles.headerRight}>
-          <Link href="resume"><a>Resume</a></Link>
-          <a href="javascript:void(0)" onClick={ () => {navIsOpen ? setNavIsOpen(false) : setNavIsOpen(true)}}>Tech Stack</a>
+      <nav className={styles.nav}>
+        <div className={styles.logo}>
+          <Link href="/">
+            <a>
+              <img src="/headerphoto.jpg" alt="Handsome" />
+            </a>
+          </Link>
         </div>
-      </header>  
-      </>
-      )
+        <ul className={mobileNavOpen ? styles.navActive : styles.navLinks}>
+          <li>
+            <Link href="resume">
+              <a>RESUME</a>
+            </Link>
+          </li>
+          <li>
+            <a
+              href="javascript:void(0)"
+              onClick={() => {
+                navIsOpen ? setNavIsOpen(false) : setNavIsOpen(true);
+              }}
+            >
+              TECH STACK
+            </a>
+          </li>
+        </ul>
+        <div
+          className={styles.burger}
+          onClick={() => {
+            setNavStyle();
+            console.log(mobileNavOpen);
+          }}
+        >
+          <div className={styles.line1}></div>
+          <div className={styles.line2}></div>
+          <div className={styles.line3}></div>
+        </div>
+      </nav>
+    </>
+  );
 }
