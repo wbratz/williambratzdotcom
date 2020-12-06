d0da1f39f34a67c9f25f96c8c876b6fc15785c16
William Bratz <williambratz615@gmail.com>
2020-12-06 10:24:22 -0500

    removes broken stuff (#16)
    Co-authored-by: Billy <williambratz00@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,6 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
-import { useEffect } from "react";
-import { useRouter } from "next/router";
-import * as gtag from "../lib/gtag";
 
 export default function App({ Component, pageProps }: AppProps) {
-  const router = useRouter();
-  useEffect(() => {
-    const handleRouteChange = (url) => {
-      gtag.pageview(url);
-    };
-    router.events.on("routeChangeComplete", handleRouteChange);
-    return () => {
-      router.events.off("routeChangeComplete", handleRouteChange);
-    };
-  }, [router.events]);
-
   return <Component {...pageProps} />;
 }
