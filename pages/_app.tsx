78fa26e484d808bce94b015ebfd402a6b94fcbdd
William Bratz <williambratz615@gmail.com>
2020-12-07 08:55:44 -0500

    finally working at basic level (#19)
    Co-authored-by: Billy <williambratz00@gmail.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,21 @@
 import "../styles/globals.css";
 import { AppProps } from "next/app";
+import { useEffect } from "react";
+import * as gtag from "../utils/gtag";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return <Component {...pageProps} />;
 }
