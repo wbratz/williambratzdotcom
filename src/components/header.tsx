ceee46b67bca143853c475198027c4903189ab21
Billy <williambratz00@gmail.com>
2020-12-07 07:54:43 -0600

    finally working at basic level

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -15,21 +15,8 @@ export default function Header({ navIsOpen, setNavIsOpen }) {
       <Head>
         <title>William Bratz dot com</title>
         <link rel="icon" href="/newIcon.ico" />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-        new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-        https://www.googletagmanager.com/gtag/js?id=UA-184834329-1'+i+dl;f.parentNode.insertBefore(j,f);
-      })(window,document,'script','dataLayer','UA-184834329-1');`,
-          }}
-        />
       </Head>
-      <noscript
-        dangerouslySetInnerHTML={{
-          __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=UA-184834329-1" height="0" width="0" style="display:none;visibility:hidden;"></iframe>`,
-        }}
-      />
+
       <nav className={styles.nav}>
         <div className={styles.logo}>
           <Link href="/">
