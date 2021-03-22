32f638e48ace58aab50700f2e581a4009599346b
William Bratz <williambratz615@gmail.com>
2021-03-21 18:21:43 -0700

    adds parallax to homepage (#29)
    bilt with to h1
    
    maybe fixes typescript error on deploy
    
    Co-authored-by: Billy Bratz <Billy.Bratz@Carvana.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,31 +5,23 @@ import React, { useEffect } from "react";
 
 export default function Home() {
   const handleScroll = () => {
-    const nameLetters = document.querySelectorAll(".nameLetters");
-    const icons = document.querySelectorAll(".icons");
+    const scroll = document.querySelectorAll(".scroll");
 
-    var nameIndex = 0,
-      length = nameLetters.length;
+    var index = 0,
+      length = scroll.length;
 
-    for (nameIndex; nameIndex < length; nameIndex++) {
-      var pos = window.pageYOffset * nameLetters[nameIndex].dataset.rate;
+    for (index; index < length; index++) {
+      var pos = window.pageYOffset * scroll[index].dataset.rate;
 
-      if (nameLetters[nameIndex].dataset.direction === "vertical") {
-        nameLetters[nameIndex].style.transform =
-          "translate3d(0px, " + pos + "px, 0px)";
-      } else {
-        nameLetters[nameIndex].style.transform =
-          "translate3d(" + pos + "px, 0px, 0px)";
+      if (scroll[index] instanceof HTMLElement) {
+        if (scroll[index].dataset.direction === "vertical") {
+          scroll[index].style.transform =
+            "translate3d(0px, " + pos + "px, 0px)";
+        } else {
+          scroll[index].style.transform =
+            "translate3d(" + pos + "px, 0px, 0px)";
+        }
       }
-    }
-
-    var iconIndex = 0,
-      length = icons.length;
-
-    for (iconIndex; iconIndex < length; iconIndex++) {
-      var pos = window.pageYOffset * icons[iconIndex].dataset.rate;
-
-      icons[iconIndex].style.transform = "translate3d(" + pos + "px, 0px, 0px)";
     }
   };
 
@@ -43,32 +35,16 @@ export default function Home() {
       <div className={styles.parallaxSection}>
         <section>
           <ul>
-            <li
-              className="nameLetters"
-              data-rate="-2"
-              data-direction="vertical"
-            >
+            <li className="scroll" data-rate="-2" data-direction="vertical">
               William{" "}
             </li>
-            <li
-              className="nameLetters"
-              data-rate="-2"
-              data-direction="horizontal"
-            >
+            <li className="scroll" data-rate="-2" data-direction="horizontal">
               Bratz{" "}
             </li>
-            <li
-              className="nameLetters"
-              data-rate="2.5"
-              data-direction="vertical"
-            >
+            <li className="scroll" data-rate="2.5" data-direction="vertical">
               dot{" "}
             </li>
-            <li
-              className="nameLetters"
-              data-rate="2"
-              data-direction="horizontal"
-            >
+            <li className="scroll" data-rate="2" data-direction="horizontal">
               com{" "}
             </li>
           </ul>
@@ -77,62 +53,59 @@ export default function Home() {
       <div className={styles.outerContainer}>
         <div className={styles.innerContainer}>
           <div className={styles.langbox}>
-            <div className="icons" data-rate=".9" data-direction="horizontal">
+            <div className="scroll" data-rate=".9" data-direction="horizontal">
               <Icon icon="Nextjs" />
             </div>
           </div>
           <div className={styles.langbox}>
-            <div className="icons" data-rate=".9" data-direction="horizontal">
+            <div className="scroll" data-rate=".9" data-direction="horizontal">
               <Icon icon="React" color="#61DAFB" />
             </div>
           </div>
           <div className={styles.langbox}>
-            <div className="icons" data-rate=".9" data-direction="horizontal">
+            <div className="scroll" data-rate=".9" data-direction="horizontal">
               <Icon icon="Typescript" color="#007acc" />
             </div>
           </div>
         </div>
         <div className={styles.innerContainer}>
           <div className={styles.langboxRight}>
-            <svg
-              className="icons"
-              data-rate="-.9"
-              data-direction="horizontal"
-              viewBox="0 0 130 130"
-            >
-              <path
-                fill="#1572B6"
-                d="M18.814 114.123l-10.054-112.771h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"
-              ></path>
-              <path
-                fill="#33A9DC"
-                d="M64.001 117.062l36.559-10.136 8.601-96.354h-45.16v106.49z"
-              ></path>
-              <path
-                fill="#fff"
-                d="M64.001 51.429h18.302l1.264-14.163h-19.566v-13.831h34.681999999999995l-.332 3.711-3.4 38.114h-30.95v-13.831z"
-              ></path>
-              <path
-                fill="#EBEBEB"
-                d="M64.083 87.349l-.061.018-15.403-4.159-.985-11.031h-13.882l1.937 21.717 28.331 7.863.063-.018v-14.39z"
-              ></path>
-              <path
-                fill="#fff"
-                d="M81.127 64.675l-1.666 18.522-15.426 4.164v14.39l28.354-7.858.208-2.337 2.406-26.881h-13.876z"
-              ></path>
-              <path
-                fill="#EBEBEB"
-                d="M64.048 23.435v13.831000000000001h-33.407999999999994l-.277-3.108-.63-7.012-.331-3.711h34.646zM64.001 51.431v13.831000000000001h-15.209l-.277-3.108-.631-7.012-.33-3.711h16.447z"
-              ></path>
-            </svg>
+            <div className="scroll" data-rate="-.9" data-direction="horizontal">
+              <svg viewBox="0 0 130 130">
+                <path
+                  fill="#1572B6"
+                  d="M18.814 114.123l-10.054-112.771h110.48l-10.064 112.754-45.243 12.543-45.119-12.526z"
+                ></path>
+                <path
+                  fill="#33A9DC"
+                  d="M64.001 117.062l36.559-10.136 8.601-96.354h-45.16v106.49z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M64.001 51.429h18.302l1.264-14.163h-19.566v-13.831h34.681999999999995l-.332 3.711-3.4 38.114h-30.95v-13.831z"
+                ></path>
+                <path
+                  fill="#EBEBEB"
+                  d="M64.083 87.349l-.061.018-15.403-4.159-.985-11.031h-13.882l1.937 21.717 28.331 7.863.063-.018v-14.39z"
+                ></path>
+                <path
+                  fill="#fff"
+                  d="M81.127 64.675l-1.666 18.522-15.426 4.164v14.39l28.354-7.858.208-2.337 2.406-26.881h-13.876z"
+                ></path>
+                <path
+                  fill="#EBEBEB"
+                  d="M64.048 23.435v13.831000000000001h-33.407999999999994l-.277-3.108-.63-7.012-.331-3.711h34.646zM64.001 51.431v13.831000000000001h-15.209l-.277-3.108-.631-7.012-.33-3.711h16.447z"
+                ></path>
+              </svg>
+            </div>
           </div>
           <div className={styles.langboxRight}>
-            <div className="icons" data-rate="-.9" data-direction="horizontal">
+            <div className="scroll" data-rate="-.9" data-direction="horizontal">
               <Icon icon="Heroku" color="#6762A6" />
             </div>
           </div>
           <div className={styles.langboxRight}>
-            <div className="icons" data-rate="-.9" data-direction="horizontal">
+            <div className="scroll" data-rate="-.9" data-direction="horizontal">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 viewBox="26 -29 256 186.355"
@@ -184,7 +157,7 @@ export default function Home() {
         </div>
       </div>
       <section></section>
-      <h1 className="nameLetters" data-rate="-.6" data-direction="vertical">
+      <h1 className="scroll" data-rate="-.6" data-direction="vertical">
         Built With
       </h1>
     </Layout>
