da5809e34e6bab1e62f7fdf5707ffc0fd87ef79a
Billy Bratz <williambratz00@gmail.com>
2023-04-25 22:02:15 -0600

    removed unused file

--- src/components/MermaidWrapper.tsx
+++
@@ -1,8 +0,0 @@
-import dynamic from 'next/dynamic';
-
-const MermaidDiagram = dynamic(
-  () => import('./MermaidDiagram').then((mod) => mod.default),
-  { ssr: false }
-);
-
-export default MermaidDiagram;
