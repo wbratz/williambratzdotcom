8c591ca86ee515578ab9fec6020b303cdf471bed
William Bratz <williambratz615@gmail.com>
2025-06-13 10:06:47 -0400

    Fix theme toggle icon visibility and dark mode footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,10 @@ import "../styles/globals.css";
 import { AppProps } from "next/app";
 import { useEffect } from "react";
 import { ThemeProvider } from "next-themes";
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
+
+config.autoAddCss = false;
 import * as gtag from "../utils/gtag";
 import { useRouter } from "next/router";
 
